1f6f91fb014564f9dfa77d968278a2eaf59fadd1
hasharas <hasharamsankalpam@gmail.com>
2025-05-03 01:23:47 +0530

    Create:pet model ,use mongoose

---
+++ pet-back/models/petModel.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+
+
+const petSchema = new mongoose.Schema({
+      name: {
+            type: String,
+            required: true,
+      },
+      age: {
+            type: Number,
+            required: true,
+      },
+      species: {
+            type: String,
+            required: true,
+      },
+      personality: {
+            type: String,
+            required: true,
+      },
+      mood: {
+            type: String,
+            required: true,
+      },
+      adopted: {
+            type: Boolean,
+            default: false,
+      },
+      adoption_date: {
+            type: Date,
+            default: null,
+      }
+
+
+})
+
+const Pet = mongoose.model('Pet', petSchema);
+export default Pet;
